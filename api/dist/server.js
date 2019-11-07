37e10c20c9857a77a18526c7eb7feb56b40d1e10
Naman Attri <namanattri5@gmail.com>
2019-11-07 13:27:17 +0530

    integrate morgan logs

--- api/dist/server.js
+++ api/dist/server.js
@@ -6,10 +6,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     result["default"] = mod;
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importStar(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
 const port = 4500;
 const app = express_1.default();
+app.use(morgan_1.default('combined'));
 app.use(express_1.json());
 app.get('/', (req, res) => res.json({ status: true, message: 'Hello world' }));
 app.listen(port, () => console.log(`api server listening on port ${port}`));
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,10 @@
 import express, { json } from 'express';
+import morgan from 'morgan';
 
 const port = 4500;
 const app = express();
+
+app.use(morgan('combined'));
 app.use(json());
 
 app.get('/', (req, res) => res.json({ status: true, message: 'Hello world'}));
